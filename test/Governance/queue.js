26c240b5c5c0d61b6744e8b9836855833a256590
Nemitari Ajienka <nemitariajienka@Taris-MacBook-Pro.local>
2021-04-06 08:08:02 +0100

    proposal queue tests for negative scenarios

--- test/Governance/queue.js
+++ test/Governance/queue.js
@@ -45,6 +45,49 @@ describe('Governance: proposal queue', () => {
 
   describe('Overlapping actions', () => {
     it('reverts on queueing overlapping actions in same proposal', async () => {
+      // delay = etherUnsigned(2 * 24 * 60 * 60).multipliedBy(2); // 2 days
+      delay = duration.minutes(2);
+
+      // Deploy Timelock contract
+      const [timelock] = await deployTimeLockHarness({delay});
+      
+      // Deploy Governance contract with timelock harness
+      [
+        gov,
+        {
+          owner,
+          timelockInstance,
+          eulerTokenInstance
+        }
+      ] = await deployGovernance({timelock});
+
+      await timelock.harnessSetAdmin(gov.address);
+      await enfranchise(eulerTokenInstance, a1, parseEther('400001'));
+
+      await mineBlock();
+
+      const targets = [eulerTokenInstance.address, eulerTokenInstance.address];
+      const values = ['0', '0'];
+      const signatures = ['getBalanceOf(address)', 'getBalanceOf(address)'];
+      const callDatas = [ethers.utils.defaultAbiCoder.encode(['address'], [a1]), ethers.utils.defaultAbiCoder.encode(['address'], [a1])];
+      await eulerTokenInstance.delegate(root);
+      await gov.propose(targets, values, signatures, callDatas, 'do nothing', {from: a1});
+      const proposalId1 = await gov.latestProposalIds(a1);
+      
+      await mineBlock();
+
+      const txVote1 = await gov.castVote(proposalId1, true, {from: a1});
+
+      //await advanceBlocks(20000)
+      for (let i = 0; i<25; i++){
+        await mineBlock();
+      }
+
+      await shouldFailWithMessage(
+        gov.queue(proposalId1, {from: a1}),
+        "revert Governance::_queueOrRevert: proposal action already queued at eta"
+      );
+
     });
 
     it('reverts on queueing overlapping actions in different proposals, works if waiting', async () => {
