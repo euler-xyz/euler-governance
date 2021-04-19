22e8ae3db24a68160ceada2dc7389d1159ddeac1
najienka <nemitariajienka@gmail.com>
2021-04-20 00:21:53 +0100

    execute proposal on payable function in target contract

--- test/Governance/proposalTargets.js
+++ test/Governance/proposalTargets.js
@@ -145,4 +145,44 @@ describe('Governance and Timelock contracts: queueTransaction, executeTransactio
 
         expectBignumberEqual(await _store.getNum(), 0);
     });
+
+    it('should revert if incorrect amount of ether is sent upon executing proposal call on payable non-view function', async () => {
+        const [
+            govInstance,
+            {
+                owner,
+                timelockInstance
+            }
+        ] = await deployGovernance(accounts);
+
+        const now = await latest();
+        const delay = await timelockInstance.delay();
+        const executionTimeStamp = now.add(delay).add(duration.minutes(1));
+
+        expect(await timelockInstance.admin()).to.be.equal(owner);
+        expect(await govInstance.guardian()).to.be.equal(owner);
+
+        const _store = await Store.new(timelockInstance.address);
+
+        expectBignumberEqual(await _store.getNum(), 0);
+        
+        const setNumber = 12;
+        const targets = _store.address;
+        const values = parseEther('1');
+        const signatures = 'paySetNum(uint256)';
+        const callDatas = ethers.utils.defaultAbiCoder.encode(['uint256'], [setNumber]);
+        await timelockInstance.queueTransaction(targets, values, signatures, callDatas, executionTimeStamp, {from: owner});
+        
+        // ensure the ETA (Unix time) has been reached in the queueTransaction() call
+        await increaseTo(executionTimeStamp);
+        //await web3.eth.sendTransaction({to:timelockInstance.address, from:owner, value:web3.utils.toWei("10", "ether")});
+        //await timelockInstance.cancelTransaction(timelockInstance.address, txFee, signature, callData, executionTimeStamp, {from: owner});
+
+        await shouldFailWithMessage(
+            timelockInstance.executeTransaction(targets, values, signatures, callDatas, executionTimeStamp, {from: owner, value: parseEther('0.1')}),
+            'Timelock::executeTransaction: Transaction execution reverted'
+        );
+
+        expectBignumberEqual(await _store.getNum(), 0);
+    });
 });
