9291250a3312851cc0eba1a4680cffe00c0a5ef5
najienka <nemitariajienka@gmail.com>
2021-09-17 08:13:39 +0100

    refactored Euler contract with access control and mock test token for access control demo

--- contracts/test/TestTokenAccessControl.sol
+++ contracts/test/TestTokenAccessControl.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Euler is ERC20Votes, AccessControl {
+contract TestToken is ERC20Votes, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     modifier onlyMinters() {
@@ -19,17 +19,19 @@ contract Euler is ERC20Votes, AccessControl {
     constructor(
         string memory name, 
         string memory symbol, 
-        uint256 totalSupply_
+        uint256 totalSupply_,
+        address minter
     ) 
         ERC20(name, symbol) 
         ERC20Permit(name) 
     {
         _mint(_msgSender(), totalSupply_);
+        _setupRole(MINTER_ROLE, minter);
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
-    function mint(address account, uint256 amount) external onlyMinters {
-        _mint(account, amount);
+    function mint(address to, uint256 amount) public onlyMinters {
+        _mint(to, amount);
     }
 
     function getChainId() external view returns (uint256) {
