43dd78eac7fe1dd903e737ce1dd5f50ef7f8d9a0
najienka <nemitariajienka@gmail.com>
2021-11-03 12:28:24 +0000

    deployment tests for vesting contract

--- test/vesting/constructor.test.js
+++ test/vesting/constructor.test.js
@@ -1,6 +1,7 @@
 const { BN, constants, expectEvent, expectRevert, time } = require('@openzeppelin/test-helpers');
 const { duration } = require('@openzeppelin/test-helpers/src/time');
 const { expect } = require('chai');
+const { expectBignumberEqual } = require('../../helpers');
 const { toBN, latest } = require('../../helpers/utils');
 const { MAX_UINT256, ZERO_ADDRESS, ZERO_BYTES32 } = constants;
 
@@ -8,14 +9,13 @@ const ERC20VotesMock = artifacts.require('ERC20VotesMock');
 const Vesting = artifacts.require('TreasuryVester');
 
 contract('TreasuryVester: constructor', function (accounts) {
-  const [owner, recipient, anotherAccount] = accounts;
+  const [owner, recipient] = accounts;
 
     const name = 'Euler';
     const symbol = 'EUL';
     const initialSupply = new BN(200);
     const vestingAmount = new BN(15);
     let now, vestingBegin, vestingCliff, vestingEnd;
-    const mintMaxPercent = toBN(2718);
 
     describe('deploy correctly and assign field variables', function () {
       beforeEach(async function () {
@@ -47,6 +47,27 @@ contract('TreasuryVester: constructor', function (accounts) {
           expect(await this.vesting.recipient()).to.not.equal(ZERO_ADDRESS);
           expect(await this.vesting.recipient()).to.equal(recipient);
         });
+
+        it('sets last update time correctly', async function () {
+          expectBignumberEqual(await this.vesting.lastUpdate(), vestingBegin);
+        })
+
+        it('sets vesting amount correctly', async function () {
+          expectBignumberEqual(await this.vesting.vestingAmount(), vestingAmount);
+        })
+
+        it('sets vesting begin timestamp correctly', async function () {
+          expectBignumberEqual(await this.vesting.vestingBegin(), vestingBegin);
+        })
+
+        it('sets vesting cliff timestamp correctly', async function () {
+          expectBignumberEqual(await this.vesting.vestingCliff(), vestingCliff);
+        })
+
+        it('sets vesting end timestamp correctly', async function () {
+          expectBignumberEqual(await this.vesting.vestingEnd(), vestingEnd);
+        })
+
 
       });
 
