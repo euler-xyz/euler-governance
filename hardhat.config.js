b4580675731dc8dd65b002ff2956cb8dee94e383
najienka <nemitariajienka@gmail.com>
2022-01-04 10:16:07 +0000

    contract byte code optimisation

--- hardhat.config.js
+++ hardhat.config.js
@@ -74,7 +74,13 @@ module.exports = {
         version: "0.8.3"
       },
       {
-        version: "0.8.4"
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+              enabled: true,
+              runs: 1,
+          },
+        }
       }
     ]
   },
@@ -83,7 +89,7 @@ module.exports = {
   },
 
   gasReporter: {
-    enabled: false
+    enabled: true
   }
 };
 
