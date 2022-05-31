80d0aad488a73be81f75ee5f450f3bb4788bb092
najienka <nemitariajienka@gmail.com>
2022-05-31 12:28:45 +0100

    update governance stub contract

--- contracts/governance/MockEulerGovernance.sol
+++ contracts/governance/MockEulerGovernance.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 contract MockEulerGovernance {
 
     event ProposalExecuted(string description, bytes proposalData);
